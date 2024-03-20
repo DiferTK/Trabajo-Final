00232ee4d9c219477d752906261bc05c1b9ce07a
DiferTK <154281253+DiferTK@users.noreply.github.com>
2024-03-20 17:56:16 -0500

    Update Vista_1.sql

--- Vistas/Vista_1.sql
+++ Vistas/Vista_1.sql
@@ -1,2 +1,9 @@
-A
-## Siss
+/*Obtener los nombres de los clientes que más gastan en alquileres*/
+'''SQL
+CREATE VIEW top_clientes_gastadores AS
+SELECT customer.customer_id, CONCAT(customer.first_name, ' ', customer.last_name) AS nombre_cliente, SUM(payment.amount) AS total_gastado
+FROM customer
+LEFT JOIN payment ON customer.customer_id = payment.customer_id
+GROUP BY customer.customer_id
+ORDER BY total_gastado DESC
+LIMIT 10;
