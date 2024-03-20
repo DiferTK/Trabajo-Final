72bfd1ecd9021cf9e91154a8e9c74137b9dfe751
DiferTK <154281253+DiferTK@users.noreply.github.com>
2024-03-20 17:56:50 -0500

    Update Vista_3.sql

--- Vistas/Vista_3.sql
+++ Vistas/Vista_3.sql
@@ -1,1 +1,9 @@
+/* Obtener el promedio de calificaciones de las películas por categoría */
+'''SQL
+CREATE VIEW promedio_calificaciones_por_categoria AS
+SELECT category.name AS categoria, AVG(film.rating) AS promedio_calificacion
+FROM category
+LEFT JOIN film_category ON category.category_id = film_category.category_id
+LEFT JOIN film ON film_category.film_id = film.film_id
+GROUP BY category.name;
 
