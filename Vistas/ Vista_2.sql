beaed4e2bc3341213df1b7a2e87b6d0da48eb944
DiferTK <154281253+DiferTK@users.noreply.github.com>
2024-03-20 17:55:41 -0500

    Update  Vista_2.sql

--- Vistas/ Vista_2.sql
+++ Vistas/ Vista_2.sql
@@ -1,1 +1,8 @@
-
+/*Obtener el número total de películas en cada categoría*/
+'''SQL
+CREATE VIEW total_peliculas_por_categoria AS
+SELECT category.name AS categoria, COUNT(film.film_id) AS total_peliculas
+FROM category
+LEFT JOIN film_category ON category.category_id = film_category.category_id
+LEFT JOIN film ON film_category.film_id = film.film_id
+GROUP BY category.name;
